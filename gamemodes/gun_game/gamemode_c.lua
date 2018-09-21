4448c3346f44278bf0a489fb8087e6563fa09f8c
Shayan <hello@shayandoust.me>
2018-09-21 23:59:44 +0100

    Refactor
    Refactor by appending spaces for neatness.

--- gamemodes/gun_game/gamemode_c.lua
+++ gamemodes/gun_game/gamemode_c.lua
@@ -1,17 +1,25 @@
 local InitPos = {199.32, -935.56, 32.69}
 
+RegisterNetEvent("Gamemode:Start:4")
+
 AddEventHandler("Gamemode:Init:4", function(GamemodeData)
+    local x,y,z = table.unpack(InitPos)
+
     SetEntityCoords(PlayerPedId(), 199.32, -935.56, 32.0, 0.0, 0.0, 0.0, 0)
     N_0xd8295af639fd9cb8(PlayerPedId())
-    local x,y,z = table.unpack(InitPos)
+
     while Citizen.InvokeNative(0x470555300D10B2A5) ~= 8 and Citizen.InvokeNative(0x470555300D10B2A5) ~= 10 do
         Citizen.Wait(0)
     end
+
     view1=CreateCam("DEFAULT_SCRIPTED_CAMERA", 1)
     SetCamCoord(view1, x, y, z + 3.0)
     SetCamRot(view1, -20.0, 0.0, 180.0)
     SetCamFov(view1, 45.0)
     RenderScriptCams(true, 1, 500,  true,  true)
+    
+    Wait(1000)
+    StartMain()
 
 end)
 
@@ -31,17 +39,21 @@ local WeaponLevels = {
 function StartMain()
     Wait(2500)
     RenderScriptCams(false, 1, 500,  true,  true)
+
     Citizen.CreateThread(function()
         local ShardS = Scaleform.Request("MP_BIG_MESSAGE_FREEMODE")
-        Scaleform.CallFunction(ShardS, false, "SHOW_SHARD_CENTERED_TOP_MP_MESSAGE", "~r~LEAVING AREA", "Head back to the battle!")
         local bx,by,bz = table.unpack(InitPos)
         local Blip = AddBlipForCoord(bx, by, bz)
+
+        Scaleform.CallFunction(ShardS, false, "SHOW_SHARD_CENTERED_TOP_MP_MESSAGE", "~r~LEAVING AREA", "Head back to the battle!")
         SetBlipAlpha(Blip, 0)
-        UpdateGunLevel()
+        UpdateGunLevel(2)
+
         while true do
             Citizen.Wait(0)
             if not IsEntityInArea(IsPedInAnyVehicle(PlayerPedId(), true) and GetVehiclePedIsIn(PlayerPedId(), false) or PlayerPedId(), 290.91, -858.1, 20.23, 107.82, -1003.69, 47.8, 1, 1, 1) then
                 if not end_time then end_time = GetNetworkTime() + 30000 end
+
                 if (end_time - GetNetworkTime()) > 0 then
                     GUI.MissionText("Go to the ~y~shootout.", 1, 1)
                     Scaleform.Render2D(ShardS)
@@ -68,9 +80,11 @@ function UpdateGunLevel(GunLevel)
     local OldWeapon = WeaponLevels[GunLevel - 1]
     local NewWeapon = WeaponLevels[GunLevel]
     local ped = PlayerPedId()
+
     if OldWeapon then 
         RemoveWeaponFromPed(ped, GetHashKey(OldWeapon))
     end
+
     GiveWeaponToPed(ped, GetHashKey(NewWeapon), 1000, false, true)
 end
 
@@ -82,9 +96,11 @@ end)
 
 RegisterNetEvent("gun_game:UpdateLevels")
 AddEventHandler("gun_game:UpdateLevels", function(GunData)
-    GunLevels = GunData
     local top3 = {}
+    GunLevels = GunData
+
     table.sort(GunLevels)
+    
     for k, v in pairs(GunLevels) do
         print(k, v)
         table.insert(top3, {sid = k, score = v})
