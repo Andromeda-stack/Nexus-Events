20dc61848f4ee3c410fbfa8ee610aa43c3cad12b
IceHax <vecchiotom1@gmail.com>
2018-11-13 18:19:05 +0100

    preparing for database implementation

--- main/main_s.lua
+++ main/main_s.lua
@@ -3,19 +3,3 @@ AddEventHandler("playerDropped", function()
 		TriggerEvent("Gamemode:Leave:"..v.id,source)
 	end
 end)
---[[ CreateThread(function()
-	local db = exports[GetCurrentResourceName()]
-	db:OpenDB("users",function()
-		for i=1,5000 do
-			local start = GetGameTimer()
-			db:InsertUser({id=i,ahahhahaha="idk fuck you ok?"}, function()
-				print("done",GetGameTimer()-start)
-			end)
-		end
-		local start = GetGameTimer()
-		db:GetUser(10000, function()
-			print("done",GetGameTimer()-start)
-		end)
-	end)
-	print("lul")
-end) ]]
