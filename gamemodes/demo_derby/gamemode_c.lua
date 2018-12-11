d4cde5a814f8097915882765ab26790539d523df
IceHax <vecchiotom1@gmail.com>
2018-12-11 15:29:13 +0100

    Get rid of useless files

--- gamemodes/demo_derby/gamemode_c.lua
+++
@@ -1,6 +0,0 @@
-RegisterNetEvent("Gamemode:Start:5")
-RegisterNetEvent("Gamemode:Session:5")
-RegisterNetEvent("Gamemode:FetchCoords:5")
-RegisterNetEvent("Gamemode:End:5")
-
--- TODO: Actually Write the code
--- gamemodes/demo_derby/gamemode_s.lua
+++
@@ -1,1 +0,0 @@
--- TODO: Actually Write the code
